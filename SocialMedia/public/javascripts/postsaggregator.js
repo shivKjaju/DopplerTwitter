d08954ac0199b68403bac36389d6dc9c3e9ecb99
shivk <shivkumarsagshi@gmail.com>
2019-04-13 18:31:29 -0500

    Backend and pipeline kind of working

--- SocialMedia/public/javascripts/postsaggregator.js
+++ SocialMedia/public/javascripts/postsaggregator.js
@@ -1,1 +1,26 @@
 //A single JavaScript file that contains all controllers
+var app = angular.module('dopplerTwitter', ['ngResource','ngRoute']);
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+}]);
+
+app.controller('HomeCtrl', ['$scope', '$resource', 
+    function($scope, $resource){
+        var post = $resource('/api/posts');
+        var mapFunction = function(){
+            emit(this.author, this.content);
+        };
+        var reduce = function(author, content){
+            return content.join();
+        };
+        db.posts.mapReduce(mapFunction, reduce, {out: "postResults"});
+        $scope.Posts = out;
+    }]);
--- SocialMedia/routes/posts.js
+++ SocialMedia/routes/posts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var monk = require('monk');
-var db = monk('localhost:27017/POST');
+var db = monk('localhost:27017/Post');
 
 router.get('/', function(req, res){
     var collection = db.get('posts');
