ebdfa6d56b9f8209f9eb9d445206be750612679a
shivk <shivkumarsagshi@gmail.com>
2019-04-13 15:20:16 -0500

    added routes and with api end points

--- SocialMedia/routes/posts.js
+++ SocialMedia/routes/posts.js
@@ -27,5 +27,37 @@ router.post('/', function(req, res){
     });
 });
 
+router.get('/:id', function(req, res){
+    var collection = db.get('posts');
+    /* not sure if we have to find one post with the id
+    or all the posts with the id*/
+    collection.findOne({_id: req.params.id}, function(err, posts){
+        if(err) throw err;
+        res.json(posts);
+    });
+});
+
+router.post('/:id', function(req, res){
+    var collection = db.get('posts');
+    collection.insertOne({_id: req.params.id} ,{
+        author: req.body.author,
+        content: req.body.content,
+        date: req.body.date,
+        // not sure about favourited field here
+       userMentions: req.body.userMentions
+    },
+    function(err, movie){
+        if(err) throw err;
+        res.json(movie);
+    });
+});
+
+router.delete('/:id', function(req, res){
+    var collection = db.get('posts');
+    collection.remove({_id: req.params.id}, function(err, posts){
+        if(err) throw err;
+        res.json(posts);
+    });
+});
 //module.exports must be our last line
 module.exports = router;
