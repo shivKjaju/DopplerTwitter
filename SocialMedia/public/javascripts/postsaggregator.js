6caa4a6c7221e1fc1aeba04a757a9e9027669b28
Brian Chong <bcjr@Brians-MacBook-Pro-2.local>
2019-04-13 18:32:16 -0500

    DAnk

--- SocialMedia/public/javascripts/postsaggregator.js
+++ SocialMedia/public/javascripts/postsaggregator.js
@@ -1,1 +1,25 @@
 //A single JavaScript file that contains all controllers
+var app = angular.module('MovieMania', ['ngResource','ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when('/add-movie', {
+            templateUrl: 'partials/movie-form.html',
+            controller: 'AddMovieCtrl'
+        })
+        .when('/movie/:id', {
+            templateUrl: 'partials/movie-form.html',
+            controller: 'EditMovieCtrl'
+        })
+        .when('/movie/delete/:id', {
+            templateUrl: 'partials/movie-delete.html',
+            controller: 'DeleteMovieCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
